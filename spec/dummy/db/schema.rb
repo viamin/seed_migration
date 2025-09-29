9dee701458628b7edf8cae1dca00c87fa70a1518
Bart Agapinan <bart@sonic.net>
2025-09-28 20:32:03 -0700

    chore: Downgrade ActiveRecord schema version from 7.2 to 7.0 for compatibility

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2014_04_02_155142) do
+ActiveRecord::Schema[7.0].define(version: 2014_04_02_155142) do
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
